7157b5044ccff3688b1dcbd3d8c24aa3509e12a4
MOhd Shameem <shameem.webdesigner@gmail.com>
2026-08-11 22:15:50 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import React from 'react'
+import TopHeader from './components/topheader/TopHeader'
+
+const App = () => {
+  return (
+    <div>
+<<<<<<< HEAD
+      <h1 className='header'>App</h1>
+=======
+      <h1>App</h1>
+      <TopHeader/>
+>>>>>>> 275ff10c59274c825cdccfec545f16c2ef196aeb
+    </div>
+  )
+}
+export default App
---
+++ src/routes/AppRoutes.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import MainLayouts from '../layouts/MainLayouts'
+import Home from '../pages/Home'
+
+const AppRoutes = () => {
+  return (
+      <Routes>
+        <Route element={<MainLayouts/>}>
+        <Route path='/' element={<Home/>} />
+        <Route/>
+      </Routes>
+  )
+}
+
+export default AppRoutes
