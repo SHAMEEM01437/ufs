8e8f2d028d246d69b938a21e4ba0b7302ee8278c
Shahne Alam <shahne.developer@gmail.com>
2026-08-27 00:03:42 +0530

    all file done

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
+// import App from "./App.jsx";
 import {BrowserRouter} from "react-router-dom"
 import './assets/css/style.css'
 import './assets/fonts/font-style.css'
+import AppRoutes from "./routes/AppRoutes.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+    <AppRoutes/>
     </BrowserRouter>
   </StrictMode>
 );
--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,16 +1,19 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import MainLayouts from '../layouts/MainLayouts'
-import Home from '../pages/Home'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+
+import MainLayouts from "../layouts/MainLayouts";
+import Home from "../pages/Home";
+import About from "../pages/About";
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route element={<MainLayouts />}>
-         <Route path='/' element={<Home />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
       </Route>
     </Routes>
-  )
-}
+  );
+};
 
-export default AppRoutes
+export default AppRoutes;
